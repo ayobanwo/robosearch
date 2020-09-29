8db38c13fcfe9b571a8e7e834374b9df20369dc2
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-09-30 00:11:40 +0100

    minor commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <h1>HELLO WORLD</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
