26a68a641c659cc3a2aeff3b9d3db7685d555532
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-10-02 02:21:30 +0100

    created card array components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CardList from './CardList'
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import {robots} from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-     <h1>HELLO WORLD</h1>
-  </React.StrictMode>,
+  	<CardList robots={robots} />
+  </React.StrictMode>, 
   document.getElementById('root')
 );
 
