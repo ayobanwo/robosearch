bf204427fa0b8c3c651dfe5f5601b94d1e58021b
olubanwo Ayomide <olubanwoayomide@gmail.com>
2020-10-06 23:55:39 +0100

    added scroll func

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CardList from './CardList'
 // import {robots} from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
+import Scroll from './Scroll';
 
 class App extends Component {
 	constructor(){
@@ -30,7 +31,9 @@ onSearchChange = (event) =>{
 			<div className='tc'>
 				<h1 className='f1'>RoboFriends</h1> 
 				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={filteredRobots} />
+				<Scroll>
+					<CardList robots={filteredRobots} />					
+				</Scroll>
 			</div>
 		)
 	}
